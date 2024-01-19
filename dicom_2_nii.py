dd13e56dda5b78abb19b348a45e90ce91c36db6f
piaoliusihai <35859137+piaoliusihai@users.noreply.github.com>
2024-01-18 20:51:29 -0800

    Create dicom_2_nii.py

---
+++ dicom_2_nii.py
@@ -0,0 +1,2 @@
+import dicom2nifti
+dicom2nifti.convert_directory("/home/xqm0629/Segmentation_breast_cancer_MRI/Dataset/1.3.46.670589.62.1.32203.2020.6.23.3.57.13.236.10788.14852", "/home/xqm0629/Segmentation_breast_cancer_MRI/Dataset/", compression=True)
